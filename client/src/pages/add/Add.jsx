586722515f4dcbf4a9a454d5f7f2d3e0f0c0a5ab
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-06-05 16:18:53 -0400

    Set onChange function that updates shoe object

--- client/src/pages/add/Add.jsx
+++ client/src/pages/add/Add.jsx
@@ -2,19 +2,25 @@ import React, { useState } from 'react'
 
 const Add = () => {
   const [shoe, setShoe] = useState({
-    name: "",
+    shoeName: "",
     desc: "",
     price: null,
     image: ""
   });
 
+  const handleChange = (e) => {
+    setShoe((prev) => ({...prev, [e.target.name]: e.target.value}));
+  }
+
+  console.log(shoe);
+
   return (
     <div className="form">
       <h1>Add new shoe</h1>
-        <input type="text" placeholder="Shoe name" />
-        <input type="text" placeholder="Shoe Description" />
-        <input type="text" placeholder="Shoe Price" />
-        <input type="text" placeholder="Shoe Image" />
+        <input type="text" placeholder="Shoe name" name="name" onChange={handleChange}/>
+        <input type="text" placeholder="Shoe Description" name="desc" onChange={handleChange}/>
+        <input type="text" placeholder="Shoe Price" name="price" onChange={handleChange}/>
+        <input type="text" placeholder="Shoe Image" name="image" onChange={handleChange}/>
     </div>
   )
 }
