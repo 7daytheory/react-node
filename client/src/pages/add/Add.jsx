c2def717267eee066271742737cbfe3fef1a046b
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-06-05 16:11:59 -0400

    Setup form, added state for each input inside object

--- client/src/pages/add/Add.jsx
+++ client/src/pages/add/Add.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Add = () => {
+  const [shoe, setShoe] = useState({
+    name: "",
+    desc: "",
+    price: null,
+    image: ""
+  });
+
   return (
-    <div>add</div>
+    <div className="form">
+      <h1>Add new shoe</h1>
+        <input type="text" placeholder="Shoe name" />
+        <input type="text" placeholder="Shoe Description" />
+        <input type="text" placeholder="Shoe Price" />
+        <input type="text" placeholder="Shoe Image" />
+    </div>
   )
 }
 
