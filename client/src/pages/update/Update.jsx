9bbe1874ed658909ef3818c8194a209609af4d5e
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-06-06 19:22:59 -0400

    Setup form structure, add submit fuction

--- client/src/pages/update/Update.jsx
+++ client/src/pages/update/Update.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+
+const handleClick = async (e) => {
+  e.preventDefault();
+
+  console.log("Form Clicked!");
+};
 
 const Update = () => {
   return (
-    <div>update</div>
-  )
-}
+    <div className="form">
+      <h1>Update shoe</h1>
+      <input
+        type="text"
+        placeholder="Shoe name"
+        name="title"
+      />
+      <textarea
+        rows={5}
+        type="text"
+        placeholder="Shoe Description"
+        name="desc"
+      />
+      <input
+        type="number"
+        placeholder="Shoe price"
+        name="price"
+      />
+      <input
+        type="text"
+        placeholder="Shoe Image"
+        name="image"
+      />
+      <button onClick={handleClick}>Update</button>
+      <Link to="/">See all shoes</Link>
+    </div>
+  );
+};
 
 export default Update
